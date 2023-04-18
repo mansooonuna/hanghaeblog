775e0c6e4c369216e377112d15a5ee43883afbee
mansooonuna <parkdasom95@gmail.com>
2023-04-18 11:47:34 +0900

    postRepository에 findByIdAndUserId 기능 추가

--- src/main/java/com/sparta/hanhaeblog/repository/PostRepository.java
+++ src/main/java/com/sparta/hanhaeblog/repository/PostRepository.java
@@ -4,7 +4,10 @@ import com.sparta.hanhaeblog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByCreatedAtDesc();
+
+    Optional<Post> findByIdAndUserId(Long id, Long userId);
 }
